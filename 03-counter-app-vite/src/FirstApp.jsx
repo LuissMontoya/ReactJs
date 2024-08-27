d541a22a13f1e1f102622c4f463e3eac648e7276
Luis Montoya <luis.montoya@ingeneosas282.onmicrosoft.com>
2024-08-27 11:25:36 -0500

    impresión de variables enel HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -2,10 +2,22 @@
 //     return (<h1>Hola Mundo!!</h1>);
 // }
 
+const newMessage = {
+  message:"Hola Mundo",
+  title:"Luis"
+};
+
+const getResult = (a,b) => {
+  return a+b;
+}
+
+
 export const FirstApp = () => {
   return (
     <>
-       <h1>Luis Montoya Component</h1>
+    <h1>Luis Montoya</h1>
+    {/*<h1>{ getResult(4,5) }</h1>*/}
+       {/*<code>{ JSON.stringify(newMessage) }</code>*/}
        <p>Este es un párrafo</p>
     </>
    
