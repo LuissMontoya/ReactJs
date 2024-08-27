77afcd79dac13e712ee1b3055f1f404a7d9fb1df
Luis Montoya <luis.montoya@ingeneosas282.onmicrosoft.com>
2024-08-27 11:08:07 -0500

    Se agrega app con vite

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,13 @@
+// export function App(){
+//     return (<h1>Hola Mundo!!</h1>);
+// }
+
+export const FirstApp = () => {
+  return (
+    <>
+       <h1>Luis Montoya Component</h1>
+       <p>Este es un párrafo</p>
+    </>
+   
+  )
+}
